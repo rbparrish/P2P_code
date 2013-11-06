d1e3754174153d8ea8e58159fe826df66a0e3752
robparrish <cr.parrish@gmail.com>
2013-11-05 17:57:10 -0800

    changed made today.  Appear to be done with scrubbing and transformations.  Time for normalization

--- open_subset.py
+++ open_subset.py
@@ -1,17 +1,22 @@
 import numpy as np
 import pandas as pd
-from parsers import to_num, binary_true_false
+import parsers as pa
 
-# create file that opens and subsets file, returning only entirety of file (but only opens part at a time to reduce RAM usage)
-def open_and_subset(file, type="simple", source="LC"):
+# create file that opens file, returning specified type
+def open_and_process(file, type="simple", source="LC"):
 	
+	# basic open into pandas
 	data = pd.read_csv(open(file))
 
+	if source != "LC":
+		return "prosper and other datasets are not yet integrated"
+
+	# options are to help with debugging, can re-select simpler subsetting/transformations as needed
 	if type == "simple":
 		return data
-	elif type == "feature_only":
+	elif type == "feature_subset":
 		return subset_by_feature(data, source)
-	elif type == "through_transform":
+	elif type == "transform":
 		data = subset_by_feature(data, source)
 		return transform(data)
 	else:
@@ -22,9 +27,10 @@ def subset_by_feature(db, source="LC"):
 	# take only features of interest
 	
 	if source == "LC":
-		return db.loc[:,['title','desc','is_inc_v','loan_amnt','term','int_rate','grade','home_ownership','purpose','annual_inc','loan_status']]
+		return db.loc[:,['title','desc','is_inc_v','loan_amnt','term','int_rate','grade','home_ownership','fico_range_high','fico_range_low','dti','purpose','annual_inc','emp_length','loan_status']]
 	else:
 		return "Prosper subsetting not yet complete"
+		# TO DO - how do a send up an excemption instead?
 
 
 def transform(db, source="LC"):
@@ -33,6 +39,8 @@ def transform(db, source="LC"):
 		##### complete transformation
 		# change title to title_length (NaN to 0)
 		db['title'] = db['title'].fillna("").apply(len)
+		# remove all titles over len(40); seems like not allowed
+		db = db[db['title'] <= 40]
 		# change desc to desc_length (NaN to 0)
 		db['desc'] = db['desc'].fillna("").apply(len)
 		# for purpose, NaN to "not_given"
@@ -41,79 +49,24 @@ def transform(db, source="LC"):
 		db = db.dropna(axis=0)
 		##### complete parsing 
 		# transform 'term' into continuous numeric values
-		db['term'] = db['term'].apply(to_num)
+		db['term'] = db['term'].apply(pa.to_num)
 		# transform 'int_rate' into continuous numeric values
-		db['int_rate'] = db['int_rate'].apply(to_num)
-		# transform 'emp_title' into whether or not employer's title is listed
-	elif source == "P":
-		return "data transform not yet made for Prosper"
-
+		db['int_rate'] = db['int_rate'].apply(pa.to_num)
+		# remove all with income over 200000 (outliers)
+		db = db[db['annual_inc'] <= 150000]
+		# make number of years working into continuous
+		db['emp_length'] = db['emp_length'].apply(pa.parse_employment_years)
+		# change 'is_inc_v' to numerical version of T/F
+		db['is_inc_v'] = db['is_inc_v'].apply(pa.binary_true_false)
+		# change categorical variables into dummy varialbes
+		db = pa.categorical_transform(db, 'home_ownership', 'home_own')
+		db = pa.categorical_transform(db, 'purpose')
+		db = pa.categorical_transform(db, 'grade')
+		# transoform loan_status (variable to predict)
+		db = pa.LC_status_transform(db)
+		db = pa.categorical_transform(db, 'loan_status', 'status')
+	else:
+		return "Prosper transformations not yet complete"
+		# TO DO - how do I send up an excemption instead?
 	return db
 
-
-
-'''
-	if source == "LC":
-		return db.loc[:,['title','desc','is_inc_v','loan_amnt','term','int_rate','grade','home_ownership','purpose','emp_title','annual_inc','loan_status']]
-	elif source == "P":
-		return "TBD"
-	else:
-		return "error"
-	#new = new[np.isfinite(new['FICO_NXT_GEN_V2_SCORE_VALUE'])]
-'''
-
-########################## NOTES ############################
-'''
-NOT USING these methods right now
-'''
-
-# takes pandas database and spits out subsetted for analysis
-def subset_basic(db):
-	# only using 'Booked Loans' (not 'Application No Listing', nor 'listing no loan')
-	new = db[db['DeclineGroup'] == "Booked Loan"]
-	# only use loans Prosper would currently accept (as noted in "Eligible_Oct2013v1")
-		# BECAUSE purpose is to increase performance for current investors
-	new = new[new["Eligible_Oct2013v1"] == 1]
-	return new
-
-
-'''
-LC values removing NaN from:
-	> 'is_inc_v' (3 out of 42538)
-'''
-
-
-'''
-MISC functions likely to be incorporated
-df.replace(1.5, nan, inplace=True)
-
-def subset_full_parse(db):
-	# for continuous, simple : change strings to numeric
-	# for categorical, simple: change strings to features (0,1 for each )
-	# for for categorical to continuous: make specific function for each instance (ex: for_income)
-
-
-Categories NaN replacements made:
-	> 'EmploymentStatus' NaN to 'not_listed'
-	> 'Occupation' NaN to 'not_listed'
-
-Categories I'm OK loosing NaN values from for the MVP
-	> 'AnnualIncomeRaw'
-	> FICO_NXT_GEN_V2_SCORE_VALUE
-	> 'ListingTermMonths'
-	> 'ListingCategoryName'
-	> 'ListingDescriptionLen'
-
-Categories I'm OK loosing NaN values from in final product
-	> 'LoanAmount' 
-
-Categories with no apparent NaNs (only subset explicitly checked):
-	> 'CurrentDelinquencies'
-	> 'DelinquenciesLast7Years'
-	> 'IncomeVerifiable'
-	> 'TotalCreditLines'
-
-Things to add into subset_advanced
-	- aggrigate the scores ahead of time (each feature = a unique vector for each scoring parameter)
-
-'''
--- parsers.py
+++ parsers.py
@@ -1,19 +1,6 @@
-import datetime
-import re
-
-
-def parse_employment_years(x):
-    m = re.search(r'(\<?)(\d{1,2})\+? years?', x)
-    if m:
-        if m.group(1) == '<' and m.group(2) == '1':
-            return 0
-        return int(m.group(2))
-    return None
-
-
-
-def parse_date(x, format='%Y-m-d'):
-    d = datetime.strptime(x, format)
+import datetime, re
+import numpy as np
+import pandas as pd
 
 
 def to_num(x):
@@ -22,8 +9,61 @@ def to_num(x):
     except ValueError:
         return "Error: do not proceed"
 
+
 def binary_true_false(x):
     if x != False:
         return 1
     return 0
     # note, can use '.fillna(False)' before this to have all NaN be 0(False), and everything else be 1(True)
+
+
+def parse_employment_years(x):
+    sp = re.split(" ", x)
+    if sp:
+        if sp[0] == '<':
+            return float(0.5)
+        elif sp[0] == '10+':
+            return float(10)
+        elif sp[0] == 'n/a':
+            return float(0)
+        return float(sp[0])
+    return None
+
+def categorical_transform(data_b, column_name, pref=None):
+    # column = column to transform
+    # make copy to not impact original database
+    db = data_b.copy()
+    # if not given, define the prefix as the original column name
+    if pref == None:
+        pref = column_name
+    # do get_dummies transform
+    new = pd.get_dummies(db[column_name], prefix=pref)
+    # append new columns onto db
+    for each in new.columns:
+        db[each] = new[each]
+    # delete original column
+    del db[column_name]
+    return db
+
+
+def parse_date(x, format='%Y-m-d'):
+    d = datetime.strptime(x, format)
+    return d
+
+
+def LC_status_transform(data_b):
+    db = data_b.copy()
+    ls = "loan_status"
+    # group Default and Charged Off together
+    db[ls]= db[ls].apply(replace_chTOde)
+    # get rid of unwanted variables
+    db_1 = db[db['loan_status'] == 'Fully Paid']
+    db_2 = db[db['loan_status'] == 'Default']
+    db = pd.concat([db_1, db_2])
+    return db
+
+
+def replace_chTOde(x):
+    if x == "Charged Off":
+        return "Default"
+    return x
